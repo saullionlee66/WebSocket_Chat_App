4c761737b6f75dad6c3f20ea03455dfb00234f74
Saul <saullionlee@hotmail.com>
2021-04-03 20:33:09 -0400

    commit

--- ws-chat-server/index.js
+++ ws-chat-server/index.js
@@ -2,7 +2,7 @@ const WebSocket = require('ws') ;
 const http = require('http');
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
 const webSocketServer = new WebSocket.Server({server});
 
