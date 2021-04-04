c709e1cd5b144ebe7e12c03413f2e5e777c55515
Saul <saullionlee@hotmail.com>
2021-04-03 20:56:45 -0400

    commit for heroku

---
+++ server.js
@@ -0,0 +1,34 @@
+const WebSocket = require('ws') ;
+const http = require('http');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
+const webSocketServer = new WebSocket.Server({server});
+
+webSocketServer.on('connection', webSocketClient =>{
+    // webSocketClient.send('{"Connection":"OK"}');
+
+    webSocketClient.on('message', message=>{
+        webSocketServer.clients.forEach(
+            client =>{
+                client.send(message);
+            }
+        )
+    })
+
+    webSocketClient.on('error', error=>{
+        console.log("Error: ", error);
+    })
+
+    webSocketClient.on('close', ws=>{
+        onclose();
+    })
+})
+
+server.listen(PORT, ()=>{
+    console.log(`WebSocket server started on port `+ PORT);
+})
+
+
+
